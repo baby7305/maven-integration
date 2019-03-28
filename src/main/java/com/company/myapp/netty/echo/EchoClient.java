17653ccad148cf203ed0a2ac217b0267616e0444
baby7305 <songhaibei7305@163.com>
2019-03-28 13:40:02 +0800

    改一下IP地址

--- src/main/java/com/company/myapp/netty/echo/EchoClient.java
+++ src/main/java/com/company/myapp/netty/echo/EchoClient.java
@@ -18,7 +18,7 @@ import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 public final class EchoClient {
 
     static final boolean SSL = System.getProperty("ssl") != null;
-    static final String HOST = System.getProperty("host", "127.0.0.1");
+    static final String HOST = System.getProperty("host", "172.29.3.6");
     static final int PORT = Integer.parseInt(System.getProperty("port", "8007"));
     static final int SIZE = Integer.parseInt(System.getProperty("size", "256"));
 
